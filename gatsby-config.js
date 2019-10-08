154433c62072ecc3426f6880468f2b302e441063
Link Strifer <chavarro@google.com>
2019-10-07 20:55:53 -0500

    fix metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `El Tablero Club Café`,
+    description: `Disfruta un buen café ☕️ acompañado de un gran juego 🚗`,
+    author: `@eltableroclub`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
