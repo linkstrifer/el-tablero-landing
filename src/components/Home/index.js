98cee321800d77ed5f6db51cc90d9a7653cac1e0
Link Strifer <chavarro@google.com>
2019-10-07 20:52:52 -0500

    first landing page

---
+++ src/components/Home/index.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+import Logo from '../../../../app/app/assets/icon.png'
+
+import styles from './styles.module.css'
+
+import { SOLIDS, GRADIENTS } from '../../../../app/app/lib/Colors'
+
+import SEO from '../Seo'
+
+function Home() {
+  return (
+    <section
+      className={styles.container}
+      style={{
+        backgroundColor: SOLIDS.PRIMARY,
+        background: `linear-gradient(300deg, ${GRADIENTS.PRIMARY[0]} 0%, ${
+          GRADIENTS.PRIMARY[1]
+        } 100%)`,
+      }}
+    >
+      <SEO title="HOME" />
+      <picture className={styles.logoContainer}>
+        <img
+          className={styles.logo}
+          src={Logo}
+          alt="El Tablero Club Café Logof"
+        />
+      </picture>
+    </section>
+  )
+}
+
+export default Home
